366ef120548313469d859ebe00feb30624cffb45
yuyongmao <yuyongmao@126.com>
2020-02-24 21:46:47 +0800

    feat: modal slide up

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,7 +1,9 @@
+import { Modal } from "@ant-design/react-native";
 import * as React from "react";
 import { useState } from "react";
-import { Modal, Text, TouchableHighlight, View } from "react-native";
+import { Text, TouchableHighlight, View } from "react-native";
 import { NavigationBar } from "../common/navigation-bar";
+import { navigationBarHeight, ScreenHeight } from "../common/screen-util";
 import i18n from "../translations";
 
 type State = {
@@ -17,35 +19,31 @@ export class HomeScreen extends React.Component<{}, State> {
     return (
       <View>
         <NavigationBar title={i18n.t("home")} />
-        <TaskModal />
+        <TaskAntdModal />
       </View>
     );
   }
 }
 
-export function TaskModal(): JSX.Element {
+export function TaskAntdModal(): JSX.Element {
   const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={{ marginTop: 22 }}>
+    <View style={{ marginTop: 22, backgroundColor: "red" }}>
       <Modal
-        animationType="slide"
+        animationType="slide-up"
         transparent={false}
+        popup
         visible={modalVisible}
-        presentationStyle={"pageSheet"}
-        onDismiss={() => {
-          setModalVisible(false);
-        }}
-        onRequestClose={() => {
+        onClose={() => {
           setModalVisible(false);
         }}
       >
-        <View style={{ marginTop: 22 }}>
+        <View style={{ height: ScreenHeight - navigationBarHeight + 30 }}>
           <View>
             <Text>Hello World!</Text>
-
             <TouchableHighlight
               onPress={() => {
-                setModalVisible(!modalVisible);
+                setModalVisible(false);
               }}
             >
               <Text>Hide Modal</Text>
@@ -72,3 +70,53 @@ export function TaskModal(): JSX.Element {
     </View>
   );
 }
+
+// export function TaskModal(): JSX.Element {
+//   const [modalVisible, setModalVisible] = useState(false);
+//   return (
+//     <View style={{ marginTop: 22 }}>
+//       <Modal
+//         animationType="slide"
+//         transparent={false}
+//         visible={modalVisible}
+//         presentationStyle={"pageSheet"}
+//         onDismiss={() => {
+//           setModalVisible(false);
+//         }}
+//         onRequestClose={() => {
+//           setModalVisible(false);
+//         }}
+//       >
+//         <View style={{ marginTop: 22 }}>
+//           <View>
+//             <Text>Hello World!</Text>
+
+//             <TouchableHighlight
+//               onPress={() => {
+//                 setModalVisible(!modalVisible);
+//               }}
+//             >
+//               <Text>Hide Modal</Text>
+//             </TouchableHighlight>
+//           </View>
+//         </View>
+//       </Modal>
+
+//       <TouchableHighlight
+//         onPress={() => {
+//           setModalVisible(true);
+//         }}
+//       >
+//         <Text>Show Modal</Text>
+//       </TouchableHighlight>
+
+//       <TouchableHighlight
+//         onPress={() => {
+//           setModalVisible(false);
+//         }}
+//       >
+//         <Text>Hide Modal</Text>
+//       </TouchableHighlight>
+//     </View>
+//   );
+// }
