69a02083fce0d6c69320cdf0d93f7afac9841501
Tian Pan <puncsky@gmail.com>
2020-02-24 01:46:37 -0800

    add task modal

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { View } from "react-native";
+import { useState } from "react";
+import { Modal, Text, TouchableHighlight, View } from "react-native";
 import { NavigationBar } from "../common/navigation-bar";
 import i18n from "../translations";
 
@@ -16,7 +17,58 @@ export class HomeScreen extends React.Component<{}, State> {
     return (
       <View>
         <NavigationBar title={i18n.t("home")} />
+        <TaskModal />
       </View>
     );
   }
+}
+
+export function TaskModal(): JSX.Element {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={{ marginTop: 22 }}>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        presentationStyle={"pageSheet"}
+        onDismiss={() => {
+          setModalVisible(false);
+        }}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={{ marginTop: 22 }}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text>Show Modal</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        onPress={() => {
+          setModalVisible(false);
+        }}
+      >
+        <Text>Hide Modal</Text>
+      </TouchableHighlight>
+    </View>
+  );
 }
